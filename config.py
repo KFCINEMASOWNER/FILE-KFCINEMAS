295e95fcdfebd57076ec5ff1a62398605984ec32
KFCINEMASOWNER <ujwalnayak25@gmail.com>
2025-01-26 18:32:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -80,7 +80,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "-1002237896833")
+URL = environ.get("URL", "")
 
 
 # Don't Remove Credit Tg - @VJ_Botz
